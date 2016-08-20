5cf945d1c33c131d89047e1f686e24b202c4900f
Gordon Adams <gordon@localhost.localdomain>
2016-08-21 09:31:21 +1200

    Added region and sub region graphs

--- jobSearch/public/javascripts/charts.js
+++ jobSearch/public/javascripts/charts.js
@@ -4,4 +4,30 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   debugger;
 
+  var regionPieDim = ndx.dimension(function(d){return d.Region});
+
+  var regionPieGroup = regionPieDim.group().reduceSum(function(d){return 1});
+
+  var regionPiechart = dc.pieChart("#majorRegion");
+
+  regionPiechart
+    .group(regionPieGroup)
+    .dimension(regionPieDim)
+
+  var suburbPieDim = ndx.dimension(function(d){return d.Suburb});
+
+  var suburbPieGroup = suburbPieDim.group().reduceSum(function(d){return 1});
+
+  var suburbPiechart = dc.pieChart("#suburb");
+
+  suburbPiechart
+    .group(suburbPieGroup)
+    .dimension(suburbPieDim)
+
+
+
+
+
+  dc.renderAll()
+
 });
